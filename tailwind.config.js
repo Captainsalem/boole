9f1b4db88f4e647db82135a8c9bff8d879f4ce22
artemis37 <57583117+Captainsalem@users.noreply.github.com>
2024-04-25 08:55:08 +0300

    Remove Custom Color Schemes from tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,57 +4,28 @@ module.exports = {
   darkMode: "class",
   theme: {
     extend: {
-      colors: {
-        primary: {
-          DEFAULT: "#96AFE2",
-          50: "#FFFFFF",
-          100: "#EEF9FE",
-          200: "#CBD0DC",
-          300: "#AEB2C5",
-          400: "#9597AF",
-          500: "#7E7F9C ",
-          600: "#696A8B",
-          700: "#56587C",
-          800: "#43476F ",
-          900: "#303763",
-          950: "#1D2857",
-        },
-        secondary: {
-          DEFAULT: "#8387ad",
-          50: "#FFFFFF",
-          100: "#ececf2",
-          200: "#d4d5e3",
-          300: "#afb1ca",
-          400: "#8387ad",
-          500: "#636894",
-          600: "#4f517a",
-          700: "#414363",
-          800: "#383954",
-          900: "#323348",
-          950: "#07070a",
-        },
-        aspectRatio: {
-          '4/3': '4 / 3',
-          '5/4': '5 / 4',
-        },
-        zIndex: {
-          '2': '2',
-          '1': '1',
-          '9': '9'
-        },
-        rotate: {
-          '-220': '-220deg',
-          '-155': '-155deg',
-          '-147': '-147deg'
-        },
-        backgroundSize: {
-          '2': '2px',
-        },
-        maxWidth: {
-          '41': '41%',
-        },
+      colors: {},
+      aspectRatio: {
+        '4/3': '4 / 3',
+        '5/4': '5 / 4',
       },
-    },
+      zIndex: {
+        '2': '2',
+        '1': '1',
+        '9': '9'
+      },
+      rotate: {
+        '-220': '-220deg',
+        '-155': '-155deg',
+        '-147': '-147deg'
+      },
+      backgroundSize: {
+        '2': '2px',
+      },
+      maxWidth: {
+        '41': '41%',
+      },
+    }
   },
   plugins: [
     require('@tailwindcss/typography'),
