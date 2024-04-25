ba75e68f2a6ab60f4a6c1f8de0f1e3cadffed15c
artemis37 <57583117+Captainsalem@users.noreply.github.com>
2024-04-25 11:36:51 +0300

    Color Customization Nonsense.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,34 +5,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: {
-          DEFAULT: "#96AFE2",
-          50: "#FFFFFF",
-          100: "#EEF9FE",
-          200: "#CBD0DC",
-          300: "#AEB2C5",
-          400: "#9597AF",
-          500: "#7E7F9C ",
-          600: "#696A8B",
-          700: "#56587C",
-          800: "#43476F ",
-          900: "#303763",
-          950: "#1D2857",
-        },
-        secondary: {
-          DEFAULT: "#8387ad",
-          50: "#FFFFFF",
-          100: "#ececf2",
-          200: "#d4d5e3",
-          300: "#afb1ca",
-          400: "#8387ad",
-          500: "#636894",
-          600: "#4f517a",
-          700: "#414363",
-          800: "#383954",
-          900: "#323348",
-          950: "#07070a",
-        },
+        primary: 'var(--ghost-accent-color)',
+        secondary: 'var(--secondary)'
+      },
         aspectRatio: {
           '4/3': '4 / 3',
           '5/4': '5 / 4',
@@ -55,7 +30,6 @@ module.exports = {
         },
       },
     },
-  },
   plugins: [
     require('@tailwindcss/typography'),
   ],
